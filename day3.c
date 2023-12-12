f3b27bc53864d859a6a4f9c313f3de72e057bb48
Dylan Roberts <dylangardnerroberts@gmail.com>
2023-12-12 15:30:33 -0500

    day3

--- day3.c
+++ day3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MAX_LINE 140
+#define MAX_LINE 140 
 
 int is_period(int c) {
     return c == 46;
@@ -14,39 +14,110 @@ int is_digit(int c) {
     return c >= 48 && c <= 57;
 }
 
-// int buildint(char grid[][MAX_LINE], int i, int j) {
-
-// void lookaround(char grid[][MAX_LINE], struct point p) {
-//     int up = grid[i - 1][j];
-//     int down = grid[i + 1][j];
-//     int left = grid[i][j - 1];
-//     int right = grid[i][j + 1];
-    
-//     return buildint(grid, i, j) + buildint(right, i, j) + probeint(up) + probeint(down)
-// }
+int atoi(char s[], int i);
 
 int main() 
 {
     int c, lc, wc, sum;
-    char grid[MAX_LINE][MAX_LINE];
-    sum = lc = 0;
+    char grid[MAX_LINE][MAX_LINE + 1];
+    wc = sum = lc = 0;
 
     while ((c = getchar()) != EOF) {
-        if (c == '\n') {lc++; wc = 0; continue;}
         grid[lc][wc++] = c;
-    }
-
-    for (int i = 0; i < MAX_LINE; i++) {
-        putchar('\n');
-        for (int j = 0; j < MAX_LINE; j++) {
-            int c = grid[i][j];
-            if (is_special(c) && !is_period(c)) {
-                putchar(c);
-                // struct point p = {i, j};
-                // sum+=lookaround(grid, p);
-            }
-            // else if (c != 46) putchar(c);
+        if (c == '\n') { 
+            lc++; 
+            wc = 0; 
         }
     }
+
+    for (int row = 0; row < MAX_LINE; row++) {
+        putchar('\n');
+        for (int col = 0; col < MAX_LINE; col++) {
+            int c = grid[row][col];
+            putchar(c);
+            if (is_special(c) && !is_period(c)) {
+                if (col && is_digit(grid[row][col-1])) { // look left
+                    int k = col - 1;
+                    while (k >= 0 && is_digit(grid[row][k])) {
+                        k--;
+                    }
+                    sum += atoi(grid[row], ++k);
+                }
+                if (col < MAX_LINE && is_digit(grid[row][col + 1])) { // look right
+                    int k = col + 1;
+                    sum += atoi(grid[row], k);
+                }
+                // look up
+                if (row && is_digit(grid[row - 1][col])) {
+                    // if I see a digit, build the full int and += sum
+                    int k = col - 1;
+                    char *focusRow = grid[row - 1]; 
+                    while (k >= 0 && is_digit(focusRow[k])) {
+                        k--;
+                    }
+                    sum += atoi(focusRow, ++k);
+                } else {
+                    if (row && col) { // up left
+                        char *focusRow = grid[row - 1];
+                        // look up left
+                        int k = col - 1; 
+                        while (k >= 0 && is_digit(focusRow[k])) {
+                            k--;
+                        }
+                        sum += atoi(focusRow, ++k);
+                    }
+
+                    if (row && col + 1 < MAX_LINE) { // up right
+                        char *focusRow = grid[row - 1];
+                        if (is_digit(focusRow[col + 1])) {
+                            int k = col + 1;
+                            sum += atoi(focusRow, k);
+                        }
+                    }
+                }
+
+                // look down
+                if (row + 1  < MAX_LINE && is_digit(grid[row + 1][col])) {
+                    // if I look down and see a digit build the full int and += sum
+                    int k = col - 1;
+                    char *focusRow = grid[row + 1];
+                    while (k >= 0 && is_digit(focusRow[k])) {
+                        k--;
+                    }
+                    sum += atoi(focusRow, ++k);
+                } else {
+                    if (row + 1 < MAX_LINE && col) { // down left
+                        // look down left
+                        char *focusRow = grid[row + 1];
+                        int k = col - 1; 
+                        while (k >= 0 && is_digit(focusRow[k])) {
+                            k--;
+                        }
+                        sum += atoi(focusRow, ++k);
+                    }
+
+                    if (row + 1 < MAX_LINE && col + 1 < MAX_LINE) { // down right
+                        char *focusRow = grid[row + 1];
+                        if (is_digit(focusRow[col + 1])) {
+                            int k = col + 1;
+                            sum += atoi(focusRow, k);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    printf("sum=%d", sum);
     return 0;
+}
+
+
+int atoi(char s[], int i) {
+    int n;
+
+    for (n = 0; is_digit(s[i]); i++)
+        n = 10 * n + (s[i] - '0');
+
+    return n;
 }
